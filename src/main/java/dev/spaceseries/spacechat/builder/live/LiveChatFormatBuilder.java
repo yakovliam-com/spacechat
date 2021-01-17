dca439adab32e79f4bdce5d5a1e390547084a0ac
yakovliam <jacob@yakovliam.com>
2021-01-16 23:07:15 -0500

    In the middle of migrating bungee chat api -> adventure

--- src/main/java/dev/spaceseries/spacechat/builder/live/LiveChatFormatBuilder.java
+++ src/main/java/dev/spaceseries/spacechat/builder/live/LiveChatFormatBuilder.java
@@ -2,14 +2,13 @@ package dev.spaceseries.spacechat.builder.live;
 
 import dev.spaceseries.api.util.ColorUtil;
 import dev.spaceseries.api.util.Trio;
-import dev.spaceseries.spacechat.builder.IBuilder;
 import dev.spaceseries.spacechat.model.Extra;
 import dev.spaceseries.spacechat.model.Format;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 
 public class LiveChatFormatBuilder implements IBuilder<Trio<Player, Format, String>, BaseComponent[]> {
@@ -28,27 +27,25 @@ public class LiveChatFormatBuilder implements IBuilder<Trio<Player, Format, Stri
         String message = input.getRight();
 
         // create component builder for message
-        ComponentBuilder componentBuilder = new ComponentBuilder("");
+        net.kyori.adventure.text.ComponentBuilder<TextComponent, TextComponent.Builder> componentBuilder = Component.text();
 
         // loop through format parts
         format.getFormatParts().forEach(formatPart -> {
             // create component builder
-            ComponentBuilder partComponentBuilder = new ComponentBuilder("");
+            net.kyori.adventure.text.ComponentBuilder<TextComponent, TextComponent.Builder> partComponentBuilder = Component.text();
             // if the part has "line", it is a SINGLE MiniMessage...in that case, just parse & return (continues to next part if exists, which it shouldn't)
             if (formatPart.getLine() != null) {
                 partComponentBuilder.append(
-                        BungeeComponentSerializer.get()
-                                .serialize(
-                                        MiniMessage.get()
-                                                .parse(
-                                                        PlaceholderAPI.setPlaceholders(
-                                                                player, ColorUtil.translateFromAmpersand(formatPart.getLine()), false)
-                                                        , "chat_message",
-                                                        message)
-                                )
+                        MiniMessage.get()
+                                .parse(
+                                        PlaceholderAPI.setPlaceholders(
+                                                player, ColorUtil.translateFromAmpersand(formatPart.getLine()), false)
+                                        , "chat_message",
+                                        message)
+
                 );
                 // append partComponentBuilder to main builder
-                componentBuilder.append(partComponentBuilder.create());
+                componentBuilder.append(partComponentBuilder.build());
                 return;
             }
 
@@ -58,11 +55,15 @@ public class LiveChatFormatBuilder implements IBuilder<Trio<Player, Format, Stri
             text = PlaceholderAPI.setPlaceholders(player, text, false);
 
             // build text from legacy (and replace <chat_message> with the actual message)
-            BaseComponent[] parsedText = dev.spaceseries.spacechat.util.color.ColorUtil.fromLegacyText(
-                    dev.spaceseries.spacechat.util.color.ColorUtil.translateFromAmpersand(
-                            text.replace("<chat_message>", message)
-                    )
-            );
+            TextComponent parsedText = LegacyComponentSerializer.legacyAmpersand().deserialize(dev.spaceseries.spacechat.util.color.ColorUtil.translateFromAmpersand(
+                    text.replace("<chat_message>", message)
+            ));
+
+//            BaseComponent[] parsedText = dev.spaceseries.spacechat.util.color.ColorUtil.fromLegacyText(
+//                    dev.spaceseries.spacechat.util.color.ColorUtil.translateFromAmpersand(
+//                            text.replace("<chat_message>", message)
+//                    )
+//            );
 
             /* Retaining events for MULTIPLE components */
 
