f39087ecfcf7b07c4a723c43ab3c16cfebc8ab0d
yakovliam <jacob@yakovliam.com>
2021-07-10 17:22:33 -0400

    :bug: Make Hikari shut down when reloading to preserve available server connections, Fix miscellaneous bugs

--- src/main/java/dev/spaceseries/spacechat/command/spacechat/ReloadCommand.java
+++ src/main/java/dev/spaceseries/spacechat/command/spacechat/ReloadCommand.java
@@ -25,23 +25,32 @@ public class ReloadCommand extends Command {
         // run async task
         CompletableFuture.runAsync(() -> {
             try {
-                // reload configurations
+                // load configs
                 plugin.loadConfigs();
-
-                // reload formats
+                // load formats
                 plugin.loadFormats();
+                // load chat manager
+                plugin.loadChatManager();
+                // load connection managers
+                plugin.loadSyncServices();
 
-                // reload channels
+                // initialize sync services, since they have to be instantiated and used after the chat manager
+                // is created
+                plugin.getChatManager().initSyncServices();
+
+                // load channels
                 plugin.loadChannels();
 
-                // reload storage
+                // close the storage connection pool (if applicable)
+                if (plugin.getStorageManager() != null && plugin.getStorageManager().getCurrent() != null) {
+                    plugin.getStorageManager().getCurrent().close();
+                }
+
+                // load storage
                 plugin.loadStorage();
+                // load users
+                plugin.loadUsers();
 
-                // load messages
-                plugin.loadMessages();
-
-                // load dynamic connections
-                plugin.loadSyncServices();
             } catch (Exception e) {
                 Messages.getInstance(plugin).reloadFailure.msg(sender);
                 e.printStackTrace();
