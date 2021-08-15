94070153dc47f390f1a539bdb62edf9272413a5c
Jacob <jacob@yakovliam.com>
2021-08-14 20:48:55 -0400

    Fix issue with subcommands registering as base commands

--- src/main/java/dev/spaceseries/spacechat/command/IgnoreCommand.java
+++ src/main/java/dev/spaceseries/spacechat/command/IgnoreCommand.java
@@ -1,10 +1,7 @@
 package dev.spaceseries.spacechat.command;
 
-import co.aikar.commands.annotation.CatchUnknown;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.HelpCommand;
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.*;
 import dev.spaceseries.spacechat.Messages;
 import dev.spaceseries.spacechat.SpaceChatPlugin;
 import org.bukkit.entity.Player;
@@ -17,6 +14,56 @@ public class IgnoreCommand extends SpaceChatCommand {
 
     public IgnoreCommand(SpaceChatPlugin plugin) {
         super(plugin);
+    }
+
+    @Subcommand("add")
+    @CommandAlias("ignore")
+    @CommandPermission("space.chat.command.ignore.add")
+    public class AddCommand extends BaseCommand {
+
+        @Default
+        public void onAdd(Player player, @Single String targetName) {
+            // get user
+            plugin.getUserManager().getByName(targetName, (user -> {
+                if (user == null) {
+                    Messages.getInstance(plugin).playerNotFound.message(player);
+                    return;
+                }
+
+                // TODO Implement adding the target from the decided storage method
+            }));
+        }
+    }
+
+    @Subcommand("list")
+    @CommandAlias("ignore")
+    @CommandPermission("space.chat.command.ignore.list")
+    public class ListCommand extends BaseCommand {
+
+        @Default
+        public void onList(Player player) {
+            // TODO Depending on decided storage method, pull all ignored players and list them in a message
+        }
+    }
+
+    @Subcommand("remove")
+    @CommandAlias("ignore")
+    @CommandPermission("space.chat.command.ignore.remove")
+    public class RemoveCommand extends BaseCommand {
+
+        @Default
+        public void onRemove(Player player, @Single String targetName) {
+            // get user
+            plugin.getUserManager().getByName(targetName, (user) -> {
+                if (user == null) {
+                    Messages.getInstance(plugin).playerNotFound.message(player);
+                    return;
+                }
+
+                // TODO Implement removing the target from the decided storage method -- also check if the sender even has ignored the target
+
+            });
+        }
     }
 
     @Default
